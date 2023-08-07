a014d809b002296aeb06376abdb961bc228dd933
StelianGabrian <steliansoring@yahoo.com>
2023-08-07 22:45:57 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here to calculate the of the three digits of a number
+
+        String number = scanner.nextLine();
+        int digit1 = Integer.parseInt(number.substring(0,1));
+        int digit2 = Integer.parseInt(number.substring(1,2));
+        int digit3 = Integer.parseInt(number.substring(2,3));
+        System.out.println(digit1+digit2+digit3);
+    }
+}
